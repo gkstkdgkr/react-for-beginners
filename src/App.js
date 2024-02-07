72da2f3a3466757f00bbc64ad7e70cf93cc4a42d
gkstkdgkr <gkstkdgkr12@gmail.com>
2024-02-07 14:46:20 +0900

    study 240207
    coin tracker

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]); // ...배열 을 쓸 시 [todo, [currentArray]] 가 아닌 [todo, currentArray]가 반환됨
-    setToDo("");
-  };
+  const dollor = 1
+  const krd = dollor * 1332.21
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers/")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item,index) => (
-          <li key={index}>{item}</li> // item앞에 index +1 을 붙치면 순서를 만들 수 있음 / 그리고 12번 라인의 toDo, ...currentArray의 순서를 바꾸면 신규 요소는 아래에 추가됨
-        ))}
-      </ul>
+      <h1>The coins! {loading ? "" :`(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select> 
+          {coins.map((coin) => (
+            <option>
+              {coin.name} ({coin.symbol}) : {(coin.quotes.USD.price * krd).toLocaleString('ko-KR',{ maximumFractionDigits: 8 })} KRD
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }

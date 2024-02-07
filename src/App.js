7425b84137194b6cedcb89178df10566fb8cdf5e
gkstkdgkr <gkstkdgkr12@gmail.com>
2024-02-07 18:28:32 +0900

    study 240207
    react router dom

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
-import { useEffect, useState } from "react";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 function App() {
-  const dollor = 1
-  const krd = dollor * 1332.21
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers/")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
   return (
-    <div>
-      <h1>The coins! {loading ? "" :`(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select> 
-          {coins.map((coin) => (
-            <option>
-              {coin.name} ({coin.symbol}) : {(coin.quotes.USD.price * krd).toLocaleString('ko-KR',{ maximumFractionDigits: 8 })} KRD
-            </option>
-          ))}
-        </select>
-      )}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
